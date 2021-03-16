5fcb1547e2f48575fdcf076dd724860c3a038cf2
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-03-16 14:40:51 -0400

    code

--- fix_prog.py
+++ fix_prog.py
@@ -36,7 +36,7 @@ def analyse_prog_file(filepath, ):
     
     # read file contents
     with open(filepath, 'r') as fh:
-        data = [_.strip() for _ in fh.readlines()]
+        data = [_.strip('\n') for _ in fh.readlines()]
     
     # check file contents
     assert len(data)  % 2 == 0
