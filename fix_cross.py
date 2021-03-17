6a1a9e9e46feb118edee949712756160acf7dcc1
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-03-17 15:30:44 -0400

    adding cross-file checks

--- fix_cross.py
+++ fix_cross.py
@@ -117,7 +117,7 @@ def find_cross_desig_duplicates() :
     print('grp_names : ', grp_names)
     for i, grp_i in enumerate( grp_names ):
         for j, grp_j in enumerate( grp_names[i+1:] ):
-            print(i,j,grp_i,grp_j' ... loading...')
+            print(i,j,grp_i,grp_j,' ... loading...')
             
             # load the contents of all files in each grp into a dict
             obs_dict_grp_i = load_grp_obs( group_dict[grp_i] )
