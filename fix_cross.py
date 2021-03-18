c83bd75dd4da5b2cb98950ba7f2c51bf5b3efa63
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-03-18 09:28:00 -0400

    adding cross-file checks

--- fix_cross.py
+++ fix_cross.py
@@ -185,7 +185,7 @@ def fix_cross_desig_duplicates(save_dir):
 
     # Make a list of filenames to loop through
     dup_file_list = glob.glob( save_dir + 'cross_des_duplicates*')
-
+    print('dup_file_list',dup_file_list )
     # Loop through the files ...
     for fp in dup_file_list:
     
