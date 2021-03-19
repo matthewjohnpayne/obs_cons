9516e72fa502808bb80442d1879f89af3f518885
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-03-19 09:42:26 -0400

    adding individual checks

--- find_individual.py
+++ find_individual.py
@@ -110,6 +110,9 @@ def _check_o80parse(deduped_obs_list):
             else:
                 parseOpt(obs80str)
         except:
+            print( obs80str)
+            print( obs80str[14] in 'srvSRV' )
+            print( parseOpt(obs80str) )
             parse_problems.append( obs80str )
             
     return parse_problems
