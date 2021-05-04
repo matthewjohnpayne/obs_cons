d03f1bf15cb38faff49d17d53be769de54a6f85f
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-05-04 18:44:52 -0400

    cross

--- find_obs80bit_duplicates.py
+++ find_obs80bit_duplicates.py
@@ -23,49 +23,68 @@ def search_for_duplicates_within_chunk( lines ):
     return {k:bit_dict[k] for k in duplicate_dict}
 
 
-def search_for_duplicates_within_single_file( filepath , n=int(1e7) ):
+def get_next_chunk_from_single_file( filepath , desired_len =int(1e7) ):
 
     print('search_for_duplicates_within_single_file:',filepath)
-    cumulative_lines = 0
     duplicate_dict = {}
-    with open(filepath,'r') as f:
-        while True:
+    
+    chunk_lines = []
+    current_len = 0
         
-            # Get next chunk
-            next_n_lines = list(islice(f, n))
-            if not next_n_lines:
-                break
-                
-            # Keep a count of the lines / progress
-            cumulative_lines += len(next_n_lines)
-            print(len(next_n_lines), cumulative_lines)
+    with open(filepath,'r') as f:
+    
+        while True :
+        
+            # Get more lines from file
+            # Add to chunk
+            chunk_lines = list(islice(f, desired_len ))
             
-            # Look for duplicates within a chunk
-            duplicate_dict.update(  search_for_duplicates_within_chunk( next_n_lines ) )
-            print( len(duplicate_dict) )
-            for k,v in duplicate_dict.items():
-                print(k,v)
+            # Length ...
+            chunk_len     = len(chunk_lines)
+
+            # Decide next step based on lengths ...
+            if chunk_len == desired_len :
+                yield chunk_lines
+                chunk_lines = []
+                current_len = 0
             
+def get_next_chunk_from_multiple_files( filepaths ):
+
+    FINISHED = False
+    while not FINISHED:
+        chunk_lines = get_next_chunk_from_single_file( filepaths[0] )
+        FINISHED    = False if chunk_lines else True
+        print(len(chunk_lines))
     
 def search_for_cross_file_duplicates():
     pass
     
-def search_all( file_list ):
+def search_for_duplicates( file_list ):
     assert file_list != [], 'You need to input a list of files'
-    
-    # find duplicates within each file
-    for filepath in file_list:
-        search_for_duplicates_within_single_file( filepath )
+        
+    # We are going to process chunks of *N* lines at once
+    # The *N* lines may come from one or many files
+    # - We will read lines from one-or-more files until we have *N* lines in a chunk
+    # We will look for duplicates that are either intra-chunk (inside) or inter-chunk (between)
+    get_next_chunk_from_multiple_files( file_list )
+        
     
 # command-line running ...
 #----------------------------------------------------
 if __name__ == '__main__':
+
+    # --- (1) : Files from the command line ---------
     assert len(sys.argv) > 2, 'You need to input a save-dir & list of files'
     assert os.path.isdir(sys.argv[1]), f'First arg [{sys.argv[1]}] not a valid directory'
     
     file_list = [ _ for _ in sys.argv[2:]]
     for _ in file_list:
         assert os.path.isfile(_), f'{_}: not a valid filepath'
-        
-    search_all( file_list )
+
+    # --- (2) : Pre-defined list of primary data files ---------
+    #assert len(sys.argv) > 1, 'You need to input a save-dir '
+    
+    
+    # ---- Do the search for duplicates ----------
+    search_for_duplicates( file_list )
 
