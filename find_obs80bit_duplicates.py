5434f8ffe5305f4bbc456690ab7b479c37ff211e
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-05-04 15:44:10 -0400

    cross

--- find_obs80bit_duplicates.py
+++ find_obs80bit_duplicates.py
@@ -1,26 +1,50 @@
 import os, sys
 from itertools import islice
+from collections import defaultdict
 
 
 
 def search_for_duplicates_within_chunk( lines ):
-    pass
+    bit_dict       = defaultdict(list)
+    duplicate_dict = {}
+    for line in lines :
+        if line[14] not in ['s','v','r']:
+            # get the obs80 bit
+            obs80bit = line[15:56]
+            
+            # look for duplicates
+            if obs80bit in bit_dict:
+                duplicate_dict[obs80bit] = True
+                
+            # save obs80bit, whether single or duplicate
+            bit_dict[obs80bit].append(lines)
+            
+    # return just the duplicates
+    return {k:bit_dict[k] for k in duplicate_dict}
 
 
 def search_for_duplicates_within_single_file( filepath , n=int(1e7) ):
 
     print('search_for_duplicates_within_single_file:',filepath)
     cumulative_lines = 0
+    duplicate_dict = {}
     with open(filepath,'r') as f:
         while True:
+        
+            # Get next chunk
             next_n_lines = list(islice(f, n))
             if not next_n_lines:
                 break
+                
+            # Keep a count of the lines / progress
             cumulative_lines += len(next_n_lines)
             print(len(next_n_lines), cumulative_lines)
+            
+            # Look for duplicates within a chunk
+            duplicate_dict.update(  search_for_duplicates_within_chunk( lines ) )
+            print( len(duplicate_dict) )
 
-
-
+            
     
 def search_for_cross_file_duplicates():
     pass
