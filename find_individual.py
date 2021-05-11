37c1b3d9331fcdb8d47733c23e388e2007bd299e
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-05-11 16:00:04 -0400

    radec

--- find_individual.py
+++ find_individual.py
@@ -30,9 +30,222 @@ allowed_2line = { _ : True for _ in ['244', '245', '247','248','249', '250', '25
 with open ('/sa/god_fit/lib/catalogue.txt' , 'r') as fh:
     allowed_AstCat = { _[:5].strip() : True for _ in fh.readlines() }
 print('allowed_AstCat' , allowed_AstCat)
-sys.exit()
+
 # allowed Mag0-Band values
-allowed_MagBand = { _ : True for _ in []}
+# (1) Copied from here: https://www.minorplanetcenter.net/iau/info/OpticalObs.html
+# 'B', 'V', 'R','I', 'J','W', 'U', 'g', 'r', 'i', 'w', 'y', 'z'
+# Almost certainly INCOMPLETE!!!
+#
+# (2) Found in xmlto80.py
+# new_style_bands = ['Vj', 'Rc', 'Ic', 'Bj', 'Uj', 'Sg', 'Sr', 'Si', 'Sz', 'Pg', 'Pr', 'Pi', 'Pz', 'Pw', 'Py', 'Ao', 'Ac', 'Gb', 'Gr']
+# old_style_bands = ['V', 'R', 'I', 'B', 'U', 'g', 'r', 'i', 'z', 'g', 'r', 'i', 'z', 'w', 'y', 'o', 'c', 'G', 'G']
+#
+# (3) From Dave...
+'''
+[('a', 'USNOA1'), ('b', 'USNOSA1'), ('c', 'USNOA2'),
+
+            ('d', 'USNOSA2'), ('e', 'UCAC1'), ('f', 'Tyc1'),
+
+            ('g', 'Tyc2'), ('h', 'GSC1.0'), ('i', 'GSC1.1'),
+
+            ('j', 'GSC1.2'), ('k', 'GSC2.2'), ('l', 'ACT'),
+
+            ('m', 'GSCACT'), ('n', 'SDSS8'), ('o', 'USNOB1'),
+
+            ('p', 'PPM'), ('q', 'UCAC4'), ('r', 'UCAC2'),
+
+            ('s', 'USNOB2'), ('t', 'PPMXL'), ('u', 'UCAC3'),
+
+            ('v', 'NOMAD'), ('w', 'CMC14'), ('x', 'Hip2'),
+
+            ('y', 'Hip1'), ('z', 'GSC'), ('A', 'AC'),
+
+            ('B', 'SAO1984'), ('C', 'SAO'), ('D', 'AGK3'),
+
+            ('E', 'FK4'), ('F', 'ACRS'), ('G', 'LickGas'),
+
+            ('H', 'Ida93'), ('I', 'Perth70'), ('J', 'COSMOS'),
+
+            ('K', 'Yale'), ('L', '2MASS'), ('M', 'GSC2.3'),
+
+            ('N', 'SDSS7'), ('O', 'SSTRC1'), ('P', 'MPOSC3'),
+
+            ('Q', 'CMC15'), ('R', 'SSTRC4'), ('S', 'URAT1'),
+
+            ('T', 'URAT2'), ('U', 'Gaia1'), ('V', 'Gaia2'),
+
+            ('W', 'UCAC5') ]
+'''
+#
+# (4) From Federica
+'''
+W    Gaia-DR3
+X    Gaia-EDR3
+Y    UCAC-5
+Z    ATLAS-2
+'''
+#
+# (5) In formatobs.f90 (instructinos from MR)
+formatobs_data = '''
+  SELECT CASE (T12(1:K))
+    CASE ("USNO-A1.0","USNO-A 1.0","USNO - A1.0","A1.0","A 1.0","A1","A 1","USNO A1","USNO A1.0","USNO A-1.0","USNO A-1","USNO-A1","USNOA1")
+      T12A = "USNO-A1.0"
+      T1 = "a"
+    CASE ("USNO-SA1.0","USNO-SA 1.0","USNO - SA1.0","SA1.0","SA 1.0","SA1","SA 1","USNO SA1","USNO SA1.0","USNO SA-1.0","USNO SA-1","USNO-SA1","USNOSA1")
+      T12A = "USNO-SA1.0"
+      T1 = "b"
+    CASE ("USNO-A2.0","USNO-2.0","USNO-A-2.0","USNO-A 2.0","USNO - A2.0","A2.0","A 2.0","A2","A 2","USNO A2","USNO A2.0","USNO A-2.0","USNO A-2","USNO-A2","USNOA-2.0","USNO 2.0","USNOA2")
+      T12A = "USNO-A2.0"
+      T1 = "c"
+    CASE ("USNO-SA2.0","USNO-SA 2.0","USNO - SA2.0","SA2.0","SA 2.0","SA2","SA 2","USNO SA2","USNO SA2.0","USNO SA-2.0","USNO SA-2","USNO-SA2","USNOSA2")
+      T12A = "USNO-SA2.0"
+      T1 = "d"
+    CASE ("UCAC1","UCAC-1","UCAC-1.0","UCAC - 1.0","UCAC 1","UCAC 1.0")
+      T12A = "UCAC-1"
+      T1 = "e"
+    CASE ("TYCHO1","TYCHO-1","TYCHO 1","TYC 1","TYC-1","Tyc1")
+      T12A = "Tycho-1"
+      T1 = "f"
+    CASE ("TYCHO2","TYCHO-2","TYCHO 2","TYC 2","TYC-2","Tyc2")
+      T12A = "Tycho-2"
+      T1 = "g"
+    CASE ("GSC-1.0","GSC 1.0","GSC1.0")
+      T12A = "GSC-1.0"
+      T1 = "h"
+    CASE ("GSC-1.1","GSC 1.1","GSC1.1")
+      T12A = "GSC-1.1"
+      T1 = "i"
+    CASE ("GSC-1.2","GSC 1.2","GSC1.2")
+      T12A = "GSC-1.2"
+      T1 = "j"
+    CASE ("GSC-2.2","GSC 2.2","GSC2.2")
+      T12A = "GSC-2.2"
+      T1 = "k"
+    CASE ("ACT")
+      T12A = "ACT"
+      T1 = "l"
+    CASE ("GSC-ACT","GSCACT")
+      T12A = "GSC-ACT"
+      T1 = "m"
+    CASE ("SDSS-DR8","SDSS-R8","SDSS8")
+      T12A = "SDSS-DR8"
+      T1 = "n"
+    CASE ("USNO-B1.0","USNO-B 1.0","USNO - B1.0","B1.0","B 1.0","B1","B 1","USNO B1","USNO B1.0","USNO B-1.0","USNO B-1","USNO-B1","USNOB1")
+      T12A = "USNO-B1.0"
+      T1 = "o"
+    CASE ("PPM")
+      T12A = "PPM"
+      T1 = "p"
+    CASE ("UCAC4","UCAC-4","UCAC-4.0","UCAC - 4.0","UCAC 4","UCAC 4.0","USNO-UCAC4")
+      T12A = "UCAC-4"
+      T1 = "q"
+    CASE ("UCAC2","UCAC-2","UCAC-2.0","UCAC - 2.0","UCAC 2","UCAC 2.0","USNO-UCAC2")
+      T12A = "UCAC-2"
+      T1 = "r"
+    CASE ("USNO-B2.0","USNO-B 2.0","USNO - B2.0","B2.0","B 2.0","B2","B 2","USNO B2","USNO B2.0","USNO B-2.0","USNO B-2","USNO-B2","USNOB2")
+      T12A = "USNO-B2.0"
+      T1 = "s"
+    CASE ("PPMXL","PPM-XL")
+      T12A = "PPMXL"
+      T1 = "t"
+    CASE ("UCAC3","UCAC-3","UCAC-3.0","UCAC - 3.0","UCAC 3","UCAC 3.0")
+      T12A = "UCAC-3"
+      T1 = "u"
+    CASE ("NOMAD")
+      T12A = "NOMAD"
+      T1 = "v"
+    CASE ("CMC","CMC-14")
+      T12A = "CMC"
+      T1 = "w"
+    CASE ("HIPPARCOS 2","HIPPARCOS-2","HIP 2","HIP-2","Hip2")
+      T12A = "Hip-2"
+      T1 = "x"
+    CASE ("HIPPARCOS","HIP","Hip1")
+      T12A = "Hip"
+      T1 = "y"
+    CASE ("GSC")
+      T12A = "GSC"
+      T1 = "z"
+    CASE ("AC")
+      T12A = "AC"
+      T1 = "A"
+    CASE ("SAO 1984")
+      T12A = "SAO1984"
+      T1 = "B"
+    CASE ("SAO")
+      T12A = "SAO"
+      T1 = "C"
+    CASE ("AGK3")
+      T12A = "AGK3"
+      T1 = "D"
+    CASE ("FK4")
+      T12A = "FK4"
+      T1 = "E"
+    CASE ("ACRS")
+      T12A = "ACRS"
+      T1 = "F"
+    CASE ("LICK GASPRA CATALOGUE")
+      T12A = "LickGasCat"
+      T1 = "G"
+    CASE ("IDA93 CATALOGUE")
+      T12A = "Ida93 Cat"
+      T1 = "H"
+    CASE ("PERTH 70")
+      T12A = "Perth 70"
+      T1 = "I"
+    CASE ("COSMOS/UKST SOUTHERN SKY CATALOGUE")
+      T12A = "COSMOS SSC"
+      T1 = "J"
+    CASE ("YALE")
+      T12A = "Yale"
+      T1 = "K"
+    CASE ("2MASS")
+      T12A = "2MASS"
+      T1 = "L"
+    CASE ("GSC-2.3","GSC 2.3","GSC2.3")
+      T12A = "GSC-2.3"
+      T1 = "M"
+    CASE ("SDSS","SDSS-R7","SDSS-DR7","SDSS7")
+      T12A = "SDSS"
+      T1 = "N"
+    CASE ("SSTRC1","SST-RC1")
+      T12A = "SSTRC1"
+      T1 = "O"
+    CASE ("MPOSC3")
+      T12A = "MPOSC3"
+      T1 = "P"
+    CASE ("CMC-15")
+      T12A = "CMC-15"
+      T1 = "Q"
+    CASE ("SSTRC4","SST-RC4")
+      T12A = "SSTRC4"
+      T1 = "R"
+    CASE ("URAT-1","URAT1")
+      T12A = "URAT-1"
+      T1 = "S"
+    CASE ("URAT-2","URAT2")
+      T12A = "URAT-2"
+      T1 = "T"
+    CASE ("Gaia-DR1","GAIA-DR1","Gaia DR1","GAIA DR1","GAIA1")
+      T12A = "Gaia-DR1"
+      T1 = "U"
+    CASE ("Gaia-DR2","GAIA-DR2","Gaia DR2","GAIA DR2","GAIA2")
+      T12A = "Gaia-DR2"
+      T1 = "V"
+    CASE DEFAULT
+      T12A = " "
+      T1 = " "
+'''.split('\n')
+# Summary: Given the above, for now I am ussing the following is the list of allowed single-character mag-band codes
+# Not at all sure this is complete
+# Not sure whether we check the single mag-band stuff anywhere in (e.g.) autoack/processobs
+allowed_MagBand = { _ : True for _ in ['B', 'V', 'R','I', 'J','W', 'U', 'G', 'g', 'r', 'i', 'w', 'y', 'z','o','c']}
+print('allowed_MagBand' , allowed_MagBand)
+formatobs_data = [_.split('=')[1] for _ in formatobs_data is 'T1' in _ and 'T12' not in _ and '=' in _ and 'CASE' not in _ ]
+
+print('formatobs_data' , formatobs_data)
+
+sys.exit()
 
 
 
