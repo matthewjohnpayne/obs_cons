8299c76bef5143e6409ee5791d693718f8692137
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-05-11 16:02:03 -0400

    radec

--- find_individual.py
+++ find_individual.py
@@ -241,7 +241,7 @@ formatobs_data = '''
 # Not sure whether we check the single mag-band stuff anywhere in (e.g.) autoack/processobs
 allowed_MagBand = { _ : True for _ in ['B', 'V', 'R','I', 'J','W', 'U', 'G', 'g', 'r', 'i', 'w', 'y', 'z','o','c']}
 print('allowed_MagBand' , allowed_MagBand)
-formatobs_data = [_.split('=')[1].strip().strip{'"'}  for _ in formatobs_data if 'T1' in _ and 'T12' not in _ and '=' in _ and 'CASE' not in _ ]
+formatobs_data = [_.split('=')[1].strip().strip{'\"'}  for _ in formatobs_data if 'T1' in _ and 'T12' not in _ and '=' in _ and 'CASE' not in _ ]
 
 print('formatobs_data' , formatobs_data)
 
